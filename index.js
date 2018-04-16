119b66da91e52bfd03183b537ae3c28e6637cdf0
John Fernandes <john1625b@gmail.com>
2018-04-16 08:57:33 -0400

    intial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const json = require('./results-20180413-161401');
+
+// console.log(json.stringify(json));
+const arr = JSON.parse(json);
+// console.log(arr)
+arr.map(row => {
+  return {qps:row.qps, freq: row.freq}
+})
+console.log(arr);
+
+
+const data = [
+{item:1,freq:1},
+{item:2,freq:3},
+{item:3,freq:5},
+{item:4,freq:3},
+{item:5,freq:1}
+]
+
+console.log(data);
+
+let flattened = [];
+
+for (let i = 0; i < data.length; i++) {
+	for (let j = 0; j < data[i].freq; j++) {
+		flattened.push(data[i].item);
+	}
+}
+
+console.log(flattened);
+
+
