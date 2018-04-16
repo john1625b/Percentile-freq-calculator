394ba61166312605bff3c2e02cc99376c6a24388
John Fernandes <john1625b@gmail.com>
2018-04-16 11:07:36 -0400

    works

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 const json = require('./results-20180413-161401');
 
-const arr = JSON.parse(json);
+const percentileCalc = (data, percentile) => {
+	let arr = JSON.parse(data);
+	
+	arr = arr.map(row => {
+//	  delete row.host
+//	  delete row.date_interval
+//	  delete row.hour
+//	  delete row.hour
+//	  delete row.p10
+//	  delete row.p25
+//	  delete row.median
+//	  delete row.p75
+//	  delete row.p95
+//	  delete row.max
+//
+	  return row
+	})
 
-arr.map(row => {
-  return {qps:row.qps, freq: row.freq}
-})
-
-
-const data = [
-{item:1,freq:1},
-{item:2,freq:3},
-{item:3,freq:5},
-{item:4,freq:3},
-{item:5,freq:1}
-]
-
-console.log(data);
-
-let flattened = [];
-
-for (let i = 0; i < data.length; i++) {
-	for (let j = 0; j < data[i].freq; j++) {
-		flattened.push(data[i].item);
+	let flattened = [];
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr[i].freq; j++) {
+			flattened.push(arr[i].qps);
+		}
 	}
+
+	console.log(flattened);
+	flattened = flattened.sort();
+	const index = Math.ceil((flattened.length * percentile));
+	return flattened[index];
 }
 
-console.log(flattened);
+console.log(percentileCalc(json, 0.5));
 
 
+module.exports = percentileCalc;
