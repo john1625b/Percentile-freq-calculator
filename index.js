757d12448937bf2b4a65975723a9e05621a75023
John Fernandes <john1625b@gmail.com>
2018-04-16 11:08:14 -0400

    removes map

--- index.js
+++ index.js
@@ -3,21 +3,6 @@ const json = require('./results-20180413-161401');
 const percentileCalc = (data, percentile) => {
 	let arr = JSON.parse(data);
 	
-	arr = arr.map(row => {
-//	  delete row.host
-//	  delete row.date_interval
-//	  delete row.hour
-//	  delete row.hour
-//	  delete row.p10
-//	  delete row.p25
-//	  delete row.median
-//	  delete row.p75
-//	  delete row.p95
-//	  delete row.max
-//
-	  return row
-	})
-
 	let flattened = [];
 	for (let i = 0; i < arr.length; i++) {
 		for (let j = 0; j < arr[i].freq; j++) {
