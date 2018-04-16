0c3b8d9a3c560abd9bbc7832ce1d46268ab4bb0f
John Fernandes <john1625b@gmail.com>
2018-04-16 12:05:57 -0400

    adds num caster

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const json = require('./results-20180413-161401');
+const fs = require('fs');
+const _ = require('lodash');
+
 
 const percentileCalc = (data, percentile, itemName, freqName) => {
 	let arr = JSON.parse(data);
+
+	arr = arr.map(row => {
+		row[itemName] = parseInt(row[itemName])
+		row[freqName] = parseInt(row[freqName])
+		return row;
+	});
 	
 	let flattened = [];
 	for (let i = 0; i < arr.length; i++) {
@@ -10,9 +19,19 @@ const percentileCalc = (data, percentile, itemName, freqName) => {
 		}
 	}
 
-	console.log(flattened);
-	flattened = flattened.sort();
+	//flattened = flattened.sort();
+	flattened = _.sortBy(flattened)
+	console.log(flattened)
 	const index = Math.ceil((flattened.length * percentile));
+
+	fs.writeFile("./output.txt", flattened, function(err) {
+			if(err) {
+			return console.log(err);
+			}
+
+			console.log("file saved with array output");
+			});
+
 	return flattened[index];
 }
 
