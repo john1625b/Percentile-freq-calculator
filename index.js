0daa8ad60686dec3d9f6165183792b481d0b220b
John Fernandes <john1625b@gmail.com>
2018-04-16 11:11:06 -0400

    adds freq and items names

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const json = require('./results-20180413-161401');
 
-const percentileCalc = (data, percentile) => {
+const percentileCalc = (data, percentile, itemName, freqName) => {
 	let arr = JSON.parse(data);
 	
 	let flattened = [];
 	for (let i = 0; i < arr.length; i++) {
-		for (let j = 0; j < arr[i].freq; j++) {
-			flattened.push(arr[i].qps);
+		for (let j = 0; j < arr[i][freqName]; j++) {
+			flattened.push(arr[i][itemName]);
 		}
 	}
 
@@ -16,7 +16,7 @@ const percentileCalc = (data, percentile) => {
 	return flattened[index];
 }
 
-console.log(percentileCalc(json, 0.5));
+console.log(percentileCalc(json, 0.5, "qps", "freq"));
 
 
 module.exports = percentileCalc;
